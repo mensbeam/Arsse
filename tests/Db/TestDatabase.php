9d5dab249c41c109a7b7d5d8f58242e2e72b16bf
J. King <jking@jkingweb.ca>
2017-03-27 23:19:05 -0400

    Helper methods for testing Database class
    May not actually work yet

---
+++ tests/Db/TestDatabase.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace JKingWeb\NewsSync;
+
+
+class TestDatabase extends \PHPUnit\Framework\TestCase {
+    use Test\Tools, Test\Db\Tools;
+    
+    
+}
---
+++ tests/lib/Db/Tools.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+namespace JKingWeb\NewsSync\Test\Db;
+
+trait Tools {
+	function prime(\JKingWeb\NewsSync\Db\Driver $drv, array $data): bool {
+		$drv->begin();
+		foreach($data as $table => $info) {
+			$cols = implode(",", array_keys($info['columns']));
+			$bindings = array_values($info['columns']);
+			$params = implode(",", array_fill(0, sizeof($info['columns']), "?"));
+			$s = $drv->prepareArray("INSERT INTO $table($cols) values($params)", $bindings);
+			foreach($info['rows'] as $row) {
+				$this->assertEquals(1, $s->runArray($row)->changes());
+			}
+		}
+		$drv->commit();
+		return true;
+	}
+
+	function compare(\JKingWeb\NewsSync\Db\Driver $drv, array $expected): bool {
+		foreach($expected as $table => $info) {
+			$cols = implode(",", array_keys($info['columns']));
+			foreach($drv->prepare("SELECT $cols from $table")->run() as $num => $row) {
+				$row = array_values($row);
+				$assertSame($expected[$table]['rows']['$num'], $row, "Row $num of table $table does not match expectation.");
+			}
+		}
+	}
+}
