7e764020296b38bf5ffaef5fae9a918c0ca37e35
J. King <jking@jkingweb.ca>
2017-08-20 14:38:24 -0400

    Unit fixes; version bump

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ namespace JKingWeb\Arsse;
 
 const BASE = __DIR__.DIRECTORY_SEPARATOR;
 const NS_BASE = __NAMESPACE__."\\";
-const VERSION = "0.0.0";
+const VERSION = "0.1.0";
 
 require_once BASE."vendor".DIRECTORY_SEPARATOR."autoload.php";
 ignore_user_abort(true);
