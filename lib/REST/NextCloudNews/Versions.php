d1e4c6eed32d9f9578ce16bb4ae6b2a182f57a79
J. King <jking@jkingweb.ca>
2017-09-29 18:11:39 -0400

    Fix 405 response of NCN version lister

--- lib/REST/NextCloudNews/Versions.php
+++ lib/REST/NextCloudNews/Versions.php
@@ -11,7 +11,7 @@ class Versions implements \JKingWeb\Arsse\REST\Handler {
     public function dispatch(\JKingWeb\Arsse\REST\Request $req): Response {
         // if a method other than GET was used, this is an error
         if ($req->method != "GET") {
-            return new Response(405);
+            return new Response(405, "", "", ["Allow: GET"]);
         }
         if (preg_match("<^/?$>", $req->path)) {
             // if the request path is an empty string or just a slash, return the supported versions
