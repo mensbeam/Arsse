e6607453bc9dfc947e5213f86e36272a74d6eaa4
J. King <jking@jkingweb.ca>
2017-12-07 15:09:03 -0500

    Disentangle Composer dev dependencies

--- tests/test.bat
+++ tests/test.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal
 set base=%~dp0
-start /b php -n -S localhost:8000 "%base%\server.php" >nul 2>nul
+start /b php -n -S localhost:8000 "%base%server.php" >nul 2>nul
 timeout /nobreak /t 1 >nul
-php "%base%\..\vendor\phpunit\phpunit\phpunit" -c "%base%\phpunit.xml" %*
+call "%base%..\vendor\bin\phpunit" -c "%base%phpunit.xml" %*
 timeout /nobreak /t 1 >nul
 for /f "usebackq tokens=5" %%a in (`netstat -aon ^| find "LISTENING" ^| find ":8000"`) do (
     taskkill /f /pid %%a >nul
