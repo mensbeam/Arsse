27caf147df5907d85469e8ce36dd3b8e7de74347
J. King <jking@jkingweb.ca>
2018-01-02 16:53:38 -0500

    Changes to Date helper class
    - Changed 'transform' method to use ValueInfo throughout. This fixes a number of obscure bugs
    - Changed the 'add' and 'sub' methods to default to "now" rather than null. This means null passes through rather than being interpreted as the current time, to be consistent with other date tools
    - Also changed the 'add' and 'sub' methods so that they operate correctly with invalid date strings
    - Added tests for the class; improves #66
    - Modified TTRSS tests because the "iso8601" format string in ValueInfo is different from Date's older format

--- lib/Misc/Date.php
+++ lib/Misc/Date.php
@@ -6,58 +6,35 @@
 declare(strict_types=1);
 namespace JKingWeb\Arsse\Misc;
 
-class Date {
-    const FORMAT = [ // in                        out
-        'iso8601'   => ["!Y-m-d\TH:i:s",          "Y-m-d\TH:i:s\Z"       ], // NOTE: ISO 8601 dates require special input processing because of varying formats for timezone offsets
-        'iso8601m'  => ["!Y-m-d\TH:i:s.u",        "Y-m-d\TH:i:s.u\Z"     ], // NOTE: ISO 8601 dates require special input processing because of varying formats for timezone offsets
-        'microtime' => ["U.u",                    "0.u00 U"              ], // NOTE: the actual input format at the user level matches the output format; pre-processing is required for PHP not to fail
-        'http'      => ["!D, d M Y H:i:s \G\M\T", "D, d M Y H:i:s \G\M\T"],
-        'sql'       => ["!Y-m-d H:i:s",           "Y-m-d H:i:s"          ],
-        'date'      => ["!Y-m-d",                 "Y-m-d"                ],
-        'time'      => ["!H:i:s",                 "H:i:s"                ],
-        'unix'      => ["U",                      "U"                    ],
-        'float'     => ["U.u",                    "U.u"                  ],
-    ];
-    
-    public static function transform($date, string $outFormat = null, string $inFormat = null, bool $inLocal = false) {
-        $date = self::normalize($date, $inFormat, $inLocal);
-        if (is_null($date) || is_null($outFormat)) {
-            return $date;
+class Date {    
+    public static function transform($date, string $outFormat = null, string $inFormat = null) {
+        $date = ValueInfo::normalize($date, ValueInfo::T_DATE, $inFormat);
+        if (!$date) {
+            return null;
         }
-        $outFormat = strtolower($outFormat);
-        if ($outFormat=="unix") {
-            return $date->getTimestamp();
+        $out = ValueInfo::normalize($date, ValueInfo::T_STRING, null, $outFormat);
+        if($outFormat=="unix") {
+            $out = (int) $out;
+        } elseif ($outFormat=="float") {
+            $out = (float) $out;
         }
-        switch ($outFormat) {
-            case 'http':    $f = "D, d M Y H:i:s \G\M\T"; break;
-            case 'iso8601': $f = "Y-m-d\TH:i:s";          break;
-            case 'sql':     $f = "Y-m-d H:i:s";           break;
-            case 'date':    $f = "Y-m-d";                 break;
-            case 'time':    $f = "H:i:s";                 break;
-            default:        $f = $outFormat;              break;
-        }
-        return $date->format($f);
+        return $out;
     }
 
     public static function normalize($date, string $inFormat = null) {
         return ValueInfo::normalize($date, ValueInfo::T_DATE, $inFormat);
     }
 
-    public static function add(string $interval, $date = null): \DateTimeInterface {
+    public static function add(string $interval, $date = "now") {
         return self::modify("add", $interval, $date);
     }
 
-    public static function sub(string $interval, $date = null): \DateTimeInterface {
+    public static function sub(string $interval, $date = "now") {
         return self::modify("sub", $interval, $date);
     }
 
-    protected static function modify(string $func, string $interval, $date = null): \DateTimeInterface {
-        $date = self::normalize($date ?? time());
-        if ($date instanceof \DateTimeImmutable) {
-            return $date->$func(new \DateInterval($interval));
-        } else {
-            $date->$func(new \DateInterval($interval));
-            return $date;
-        }
+    protected static function modify(string $func, string $interval, $date) {
+        $date = self::normalize($date);
+        return $date ? $date->$func(new \DateInterval($interval)) : null;
     }
 }
---
+++ tests/cases/Misc/TestDate.php
@@ -0,0 +1,60 @@
+<?php
+/** @license MIT
+ * Copyright 2017 J. King, Dustin Wilson et al.
+ * See LICENSE and AUTHORS files for details */
+
+declare(strict_types=1);
+namespace JKingWeb\Arsse\TestCase\Misc;
+
+use JKingWeb\Arsse\Misc\Date;
+
+/** @covers \JKingWeb\Arsse\Misc\Date */
+class TestDate extends \JKingWeb\Arsse\Test\AbstractTest {
+    public function setUp() {
+        $this->clearData();
+    }
+
+    function testNormalizeADate() {
+        $exp = new \DateTimeImmutable("2018-01-01T00:00:00Z");
+        $this->assertEquals($exp, Date::normalize(1514764800));
+        $this->assertEquals($exp, Date::normalize("2018-01-01T00:00:00"));
+        $this->assertEquals($exp, Date::normalize("2018-01-01 00:00:00"));
+        $this->assertEquals($exp, Date::normalize("Mon, 01 Jan 2018 00:00:00 GMT", "http"));
+        $this->assertEquals($exp, Date::normalize(new \DateTime("2017-12-31 19:00:00-0500")));
+        $this->assertNull(Date::normalize(null));
+        $this->assertNull(Date::normalize("ook"));
+        $this->assertNull(Date::normalize("2018-01-01T00:00:00Z", "http"));
+    }
+
+    function testFormatADate() {
+        $test = new \DateTimeImmutable("2018-01-01T00:00:00Z");
+        $this->assertNull(Date::transform(null, "http"));
+        $this->assertNull(Date::transform("ook", "http"));
+        $this->assertNull(Date::transform("2018-01-01T00:00:00Z", "iso8601", "http"));
+        $this->assertSame("2018-01-01T00:00:00Z", Date::transform($test));
+        $this->assertSame("2018-01-01T00:00:00Z", Date::transform($test, "iso8601"));
+        $this->assertSame("Mon, 01 Jan 2018 00:00:00 GMT", Date::transform($test, "http"));
+        $this->assertSame(1514764800, Date::transform($test, "unix"));
+        $this->assertSame(1514764800.0, Date::transform($test, "float"));
+        $this->assertSame(1514764800.265579, Date::transform("0.26557900 1514764800", "float", "microtime"));
+        $this->assertSame(1514764800.265579, Date::transform("2018-01-01T00:00:00.265579Z", "float", "iso8601m"));
+    }
+
+    function testMoveDateForward() {
+        $test = new \DateTimeImmutable("2018-01-01T00:00:00Z");
+        $this->assertNull(Date::add("P1D", null));
+        $this->assertNull(Date::add("P1D", "ook"));
+        $this->assertEquals($test->add(new \DateInterval("P1D")), Date::add("P1D", $test));
+        $this->assertException();
+        $this->assertNull(Date::add("ook", $test));
+    }
+
+    function testMoveDateBack() {
+        $test = new \DateTimeImmutable("2018-01-01T00:00:00Z");
+        $this->assertNull(Date::sub("P1D", null));
+        $this->assertNull(Date::sub("P1D", "ook"));
+        $this->assertEquals($test->sub(new \DateInterval("P1D")), Date::sub("P1D", $test));
+        $this->assertException();
+        $this->assertNull(Date::sub("ook", $test));
+    }
+}
