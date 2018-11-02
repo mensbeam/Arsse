2dd1b45d3e650b783d0093b7620a8344f496570e
J. King <jking@jkingweb.ca>
2018-11-02 12:14:46 -0400

    Cover the Query class with database tests for now

--- tests/cases/Db/SQLite3PDO/Database/TestFolder.php
+++ tests/cases/Db/SQLite3PDO/Database/TestFolder.php
@@ -6,7 +6,10 @@
 declare(strict_types=1);
 namespace JKingWeb\Arsse\TestCase\Db\SQLite3PDO\Database;
 
-/** @covers \JKingWeb\Arsse\Database<extended> */
+/**
+ * @covers \JKingWeb\Arsse\Database<extended>
+ * @covers \JKingWeb\Arsse\Misc\Query
+ */
 class TestFolder extends \JKingWeb\Arsse\Test\AbstractTest {
     use \JKingWeb\Arsse\Test\Database\Setup;
     use \JKingWeb\Arsse\Test\Database\DriverSQLite3PDO;
--- tests/cases/Db/SQLite3PDO/Database/TestSession.php
+++ tests/cases/Db/SQLite3PDO/Database/TestSession.php
@@ -2,7 +2,10 @@
 declare(strict_types=1);
 namespace JKingWeb\Arsse\TestCase\Db\SQLite3PDO\Database;
 
-/** @covers \JKingWeb\Arsse\Database<extended> */
+/**
+ * @covers \JKingWeb\Arsse\Database<extended>
+ * @covers \JKingWeb\Arsse\Misc\Query
+ */
 class TestSession extends \JKingWeb\Arsse\Test\AbstractTest {
     use \JKingWeb\Arsse\Test\Database\Setup;
     use \JKingWeb\Arsse\Test\Database\DriverSQLite3PDO;
