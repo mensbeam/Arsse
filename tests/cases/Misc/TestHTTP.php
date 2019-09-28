4f5a8e3180bd84b10a1e2dd21b03bad16c08b249
J. King <jking@jkingweb.ca>
2019-09-27 22:38:03 -0400

    Make media type checking more robust

---
+++ tests/cases/Misc/TestHTTP.php
@@ -0,0 +1,32 @@
+<?php
+/** @license MIT
+ * Copyright 2017 J. King, Dustin Wilson et al.
+ * See LICENSE and AUTHORS files for details */
+
+declare(strict_types=1);
+namespace JKingWeb\Arsse\TestCase\Misc;
+
+use JKingWeb\Arsse\Misc\HTTP;
+use Psr\Http\Message\ResponseInterface;
+
+/** @covers \JKingWeb\Arsse\Misc\HTTP */
+class TestHTTP extends \JKingWeb\Arsse\Test\AbstractTest {
+    /** @dataProvider provideMediaTypes */
+    public function testMatchMediaType(string $header, array $types, bool $exp) {
+        $msg = (new \Zend\Diactoros\Request)->withHeader("Content-Type", $header);
+        $this->assertSame($exp, HTTP::matchType($msg, ...$types));
+        $msg = (new \Zend\Diactoros\Response)->withHeader("Content-Type", $header);
+        $this->assertSame($exp, HTTP::matchType($msg, ...$types));
+    }
+
+    public function provideMediaTypes() {
+        return [
+            ["application/json",         ["application/json"],              true],
+            ["APPLICATION/JSON",         ["application/json"],              true],
+            ["text/JSON",                ["application/json", "text/json"], true],
+            ["text/json; charset=utf-8", ["application/json", "text/json"], true],
+            ["",                         ["application/json"],              false],
+            ["",                         ["application/json", ""],          true],
+        ];
+    }
+}
