9cb7cf485df0452bea335ed94b22d1282fab4079
J. King <jking@jkingweb.ca>
2020-01-23 17:07:58 -0500

    Style fix

--- tests/docroot/Feed/Caching/304Conditional.php
+++ tests/docroot/Feed/Caching/304Conditional.php
@@ -1,9 +1,9 @@
 <?php
-// this test returns 400 rather than 304 if the values of If-Modified-Since 
+// this test returns 400 rather than 304 if the values of If-Modified-Since
 // and If-None-Match doesn't match $G_GET['t'] and $_GET['e'] respectively, or
 // if the $_GET members are missing
 if (
-    !($_GET['t'] ?? "") || 
+    !($_GET['t'] ?? "") ||
     !($_GET['e'] ?? "") ||
     ($_SERVER['HTTP_IF_MODIFIED_SINCE'] ?? "") !== gmdate("D, d M Y H:i:s \G\M\T", (int) $_GET['t']) ||
     ($_SERVER['HTTP_IF_NONE_MATCH'] ?? "") !== $_GET['e']
@@ -20,4 +20,3 @@ if (
         ],
     ];
 }
-    
