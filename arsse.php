3ba82b7c6da01d961990dc5ba872e1449ff9eb47
J. King <jking@jkingweb.ca>
2021-02-10 22:12:00 -0500

    Fix CLI bootstrap problem

--- arsse.php
+++ arsse.php
@@ -15,7 +15,8 @@ ini_set("memory_limit", "-1");
 ini_set("max_execution_time", "0");
 
 if (\PHP_SAPI === "cli") {
-    // initialize the CLI; this automatically handles --help and --version
+    // initialize the CLI; this automatically handles --help and --version else {
+    Arsse::$obj = new Factory;
     $cli = new CLI;
     // handle other CLI requests; some do not require configuration
     $exitStatus = $cli->dispatch();
