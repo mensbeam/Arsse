a97ca2363114362c7e19502cc01dfc7792898784
J. King <jking@jkingweb.ca>
2021-05-17 18:03:47 -0400

    Don't try to enable extensions

--- dist/arch/arsse.sh
+++ dist/arch/arsse.sh
@@ -1,10 +1,3 @@
-#! /usr/bin/bash
-
-if [ `id -u` -eq 0 ]; then
-    setpriv --clear-groups --inh-caps -all --egid=arsse --euid=arsse php /usr/share/webapps/arsse/arsse.php $@
-elif [ `id -un` == "arsse" ]; then
-    php /usr/share/webapps/arsse/arsse.php $@
-else
-    echo "Not authorized." >&2
-    exit 1
-fi
+#! /usr/bin/php
+<?php
+require "/usr/share/webapps/arsse/arsse.php";
