568b12600b22a680c29d4a81776a2d242066f031
J. King <jking@jkingweb.ca>
2021-05-18 09:46:42 -0400

    Drop privileges when executing CLI

--- dist/arch/arsse.sh
+++ dist/arch/arsse.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/php
 <?php
+if (posix_geteuid() == 0) {
+    $info = posix_getpwnam("arsse");
+    if ($info) { 
+        posix_setgid($info['gid']);
+        posix_setuid($info['uid']);
+    }
+}
 require "/usr/share/webapps/arsse/arsse.php";
